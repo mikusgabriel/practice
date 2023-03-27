cfadc258f0a93576732bf6e904c9148f80fac66b
Gabriel Mikus <gabriel.mikus11@gmail.com>
2023-03-27 18:07:43 -0400

    ye

---
+++ my-app/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from '@mui/material/Button';
+import { Accordion } from '@mui/material';
+
+function App() {
+  return (
+    <>
+    <div className="App">
+      <h1>Hello World</h1>
+      <div>
+      <Button>uwu</Button>
+      </div>
+    </div>
+    </>
+  );
+}
+
+export default App;
