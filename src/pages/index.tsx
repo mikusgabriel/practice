8fefd9b10cd438d6113ab13614a02cd851a0dd49
Gabriel Mikus <gabriel.mikus11@gmail.com>
2023-03-26 23:33:32 -0400

    Ye

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,12 +6,7 @@ import Editable from "../Components/Editables/Editables";
 function App() {
   return (
     <>
-      <h1>This is a practice</h1>
-
-      <Accordion title="Product Info" content="This is product info content" ></Accordion>
-      <br />
-      <Editable></Editable>
-    </>
+     </>
   );
 }
 
